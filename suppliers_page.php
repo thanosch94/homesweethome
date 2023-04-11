4422044958cfbfec2b75682038e8968f4e734c32
Thanos Chatzopoulos <chatzopoulosthanos@gmail.com>
2023-04-11 20:41:38 +0300

    Add container class and change images to 100%

--- suppliers_page.php
+++ suppliers_page.php
@@ -14,170 +14,172 @@
     include "header.php"
     ?>
     <main>
-        <div class="container center mt-4">
+        <div class="container center mt-2">
             <h1 class="fw-bold mb-5">Προμηθευτές</h1>
         </div>
         <div>
             <h4 class="center mb-5">Εδώ θα βρείς ο,τι χρειάζεται να ξέρεις για τον κάθε προμηθευτή ξεχωριστά</h4>
         </div>
-        <div class=" w-100 mt-4 mb-5">
-            <div class="row ">
-            <div class="col-2 opacity-75 test">
-                <img src="images/suppliers/1.jpg" width="250px">
+        <div class="container">
+            <div class="w-100 mt-4 mb-5">
+                <div class="row ">
+                <div class="col-2 opacity-75 test">
+                    <img src="images/suppliers/1.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/2.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/3.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/4.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/5.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/6.jpg" width="100%">
+                </div>
             </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/2.jpg" width="250px">
+            <div class="row mt-3">
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/7.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/8.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/9.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/10.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/11.jpg" width="100%">
             </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/3.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/4.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/5.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/6.jpg" width="250px">
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/12.jpg" width="100%">
+                </div>
+                </div>
+                <div class="row mt-3">
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/13.png" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/14.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/15.png" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/16.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/17.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/18.jpg" width="100%">
+                </div>
+                </div>
+                <div class="row mt-3">
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/19.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/20.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/21.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/22.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/23.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/24.jpg" width="100%">
+                </div>
+                </div>
+                <div class="row mt-3">
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/25.png" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/26.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/27.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/28.png" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/29.jpg" width="100%">
+                </div>
+                <div class="col-2 opacity-75">
+                    <img src="images/suppliers/30.png" width="100%">
+                </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/31.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/32.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/33.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/34.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/35.jpg" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/36.png" width="100%">
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/37.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/38.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/39.jpg" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/40.jpg" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/41.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/42.jpg" width="100%">
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/43.jpg" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/44.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/45.png" width="100%">
+                    </div>
+                    <div class="col-2 opacity-75">
+                    <img src="images/suppliers/46.jpg" width="100%">
+                    </div>
+                </div>
+
             </div>
         </div>
-        <div class="row mt-3">
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/7.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/8.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/9.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/10.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/11.jpg" width="250px">
-        </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/12.jpg" width="250px">
-            </div>
-            </div>
-            <div class="row mt-3">
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/13.png" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/14.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/15.png" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/16.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/17.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/18.jpg" width="250px">
-            </div>
-            </div>
-            <div class="row mt-3">
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/19.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/20.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/21.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/22.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/23.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/24.jpg" width="250px">
-            </div>
-            </div>
-            <div class="row mt-3">
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/25.png" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/26.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/27.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/28.png" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/29.jpg" width="250px">
-            </div>
-            <div class="col-2 opacity-75">
-                <img src="images/suppliers/30.png" width="250px">
-            </div>
-            </div>
-
-            <div class="row mt-3">
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/31.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/32.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/33.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/34.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/35.jpg" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/36.png" width="250px">
-                </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/37.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/38.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/39.jpg" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/40.jpg" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/41.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/42.jpg" width="250px">
-                </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/43.jpg" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/44.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/45.png" width="250px">
-                </div>
-                <div class="col-2 opacity-75">
-                <img src="images/suppliers/46.jpg" width="250px">
-                </div>
-            </div>
-
-    </div>
     </main>
     <?php
     include "footer.php"
